91dcd47b57e90f88567dbc684b32766239bfa1a3
mauricioHidani <mhidani@student.42org.br>
2025-09-07 22:46:45 -0300

    [pipex] feat: header external includes

---
+++ pipex/includes/external_incs.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   external_incs.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/06 17:37:03 by mhidani           #+#    #+#             */
+/*   Updated: 2025/09/06 17:51:29 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EXTERNAL_INCS_H
+# define EXTERNAL_INCS_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+
+#endif
