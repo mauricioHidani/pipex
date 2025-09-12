8ca3fd6231f1b99119a8bd4e1d54916a9ed645e5
mauricioHidani <mhidani@student.42org.br>
2025-09-11 22:30:04 -0300

    [pipex] feat: definitions header

---
+++ includes/definitions.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   definitions.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/11 01:31:23 by mhidani           #+#    #+#             */
+/*   Updated: 2025/09/11 01:32:11 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEFINITIONS_H
+# define DEFINITIONS_H
+
+# ifndef HEREDOC
+#  define HEREDOC "here_doc"
+# endif
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 128
+# endif
+
+# define SUCCESS 0
+# define ERROR 1
+
+#endif
