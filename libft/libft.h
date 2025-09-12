f75ab61636a834b7e61cc65aa8f0604176fbb174
mauricioHidani <mhidani@student.42org.br>
2025-09-11 22:13:06 -0300

    [libft] feat: strcmp

--- libft/libft.h
+++ libft/libft.h
@@ -37,6 +37,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dst_size);
 size_t	ft_strlcat(char *dst, const char *src, size_t dsize);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_strnstr(const char *big, const char *little, size_t len);
