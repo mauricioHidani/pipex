8649c9d1ba8eb9be9019567a11bfc6714e4c7618
mauricioHidani <mhidani@student.42org.br>
2025-09-24 19:43:43 -0300

    refactor: prtop signs

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/21 17:57:26 by mhidani           #+#    #+#             */
-/*   Updated: 2025/09/24 12:20:12 by mhidani          ###   ########.fr       */
+/*   Updated: 2025/09/24 19:32:17 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,19 +47,18 @@ t_shrd	*ft_build_shrd(int argc, char **argv, char **envp, char ishdoc);
 int		**ft_build_pipes(int argc, char ishdoc);
 t_dlist	*ft_build_cmds(t_shrd *shared);
 
-void	ft_exec_cmds(t_dlist *cmds, int **pips, t_shrd *shrd);
-void	ft_exec_first_cmd(t_cmd *cmd, int **pips, t_shrd *shrd);
-void	ft_exec_middle_cmd(t_cmd *cmd, int **pips, size_t idx, t_shrd *shrd);
-void	ft_exec_last_cmd(t_cmd *cmd, int **pips, t_shrd *shared);
+void	ft_resolve_cmds(t_dlist *cmds, int **pips, t_shrd *shrd);
+void	ft_execproc(t_cmd *cmd, int **pips, size_t idx, t_shrd *shrd);
+void	ft_last_execproc(t_cmd *cmd, int **pips, t_shrd *shared);
 
-void	ft_handler_heredoc(char *eof, int *fd);
-void	ft_handler_infile(char *filename, int *fd);
-int		ft_handler_outfile(t_shrd *shared);
+void	ft_handle_heredoc(char *eof, int *fd);
+void	ft_handle_infile(char *filename, int *fd);
+int		ft_handle_outfile(t_shrd *shared);
 
 char	**ft_extract_paths(char **envp);
 char	*ft_extract_pcmd(char *cmd, char **paths, int *code_status);
 
-void	ft_waitpids(t_dlist *lst);
+void	ft_waitpids(t_dlist *lst, int *status);
 void	ft_close_all(int **pipes, t_shrd *shrd);
 void	ft_clean_all(t_shrd *shrd, int **pips, t_dlist *lst);
 void	ft_clean_cmd(void *cmd);
