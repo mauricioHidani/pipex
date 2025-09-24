6d90524a75496e4a404540a8dceb5cc71ba450dc
mauricioHidani <mhidani@student.42org.br>
2025-09-24 19:44:17 -0300

    feat: func resolve wait proc by pid

--- srcs/pipex/ft_waitpids.c
+++ srcs/pipex/ft_waitpids.c
@@ -6,13 +6,13 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 12:15:58 by mhidani           #+#    #+#             */
-/*   Updated: 2025/09/24 12:19:04 by mhidani          ###   ########.fr       */
+/*   Updated: 2025/09/24 19:31:42 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	ft_waitpids(t_dlist *lst)
+void	ft_waitpids(t_dlist *lst, int *status)
 {
 	t_cmd	*cmd;
 	t_dnode	*pivot;
@@ -21,7 +21,12 @@ void	ft_waitpids(t_dlist *lst)
 	while (pivot)
 	{
 		cmd = (t_cmd *)pivot->data;
-		waitpid(cmd->pid, &cmd->status, 0);
+		if (waitpid(cmd->pid, status, 0) == -1)
+			break ;
 		pivot = pivot->next;
 	}
+	if (WIFEXITED(*status))
+		*status = WEXITSTATUS(*status);
+	else if (WIFSIGNALED(*status))
+		*status = 128 + WTERMSIG(*status);
 }
